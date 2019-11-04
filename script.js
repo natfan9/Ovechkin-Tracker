ab8334eeddf0b6d00fcf932e8c14eb017956965f
natfan9 <50808373+natfan9@users.noreply.github.com>
2019-11-03 22:12:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,10 @@ request.onload = function() {
 	console.log(request.response);
 	var bunchadata = request.response;
 	var evshots = evShotsPer60(bunchadata);
-	//console.log(evshots);
 	document.getElementById("evshots").innerHTML = evshots.toPrecision(4);
+	var evdisplaytoi = evDisplayTOI(bunchadata);
+	document.getElementById("evtoi").innerHTML = evdisplaytoi;
+	var evtoi = evTOI(bunchadata);
 }
 
 function evShotsPer60(jsonObj) {
@@ -44,4 +46,47 @@ function evShotsPer60(jsonObj) {
 	
 	var avg = avgshots/avgtimehour;
 	return avg;
+}
+
+function evDisplayTOI(jsonObj) {
+	var totaltime = [];
+	for (const time in jsonObj) {
+		totaltime.push(jsonObj[time]["EV TOI"]);
+	}
+	console.log(totaltime);
+
+	var period = 82;
+	
+	var timesum = 0;
+	for (var b = 0; b < period; b++) {
+    	timesum += totaltime[totaltime.length-period+b];
+	}
+	var avgtime = timesum/period;
+	console.log(avgtime);
+	
+	var hours = Math.floor(avgtime / 3600);
+	avgtime %= 3600;
+	var minutes = Math.floor(avgtime / 60);
+	var seconds = avgtime % 60;
+	
+	return hours + ":" + minutes + ":" + seconds;
+}
+
+function evTOI(jsonObj) {
+	var totaltime = [];
+	for (const time in jsonObj) {
+		totaltime.push(jsonObj[time]["EV TOI"]);
+	}
+	console.log(totaltime);
+
+	var period = 82;
+	
+	var timesum = 0;
+	for (var b = 0; b < period; b++) {
+    	timesum += totaltime[totaltime.length-period+b];
+	}
+	var avgtime = timesum/period;
+	console.log(avgtime);
+	
+	return avgtime;
 }
