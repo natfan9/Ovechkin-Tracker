1eebdd01a3e8900ed664be05855687f4c348f469
natfan9 <50808373+natfan9@users.noreply.github.com>
2019-11-03 21:44:20 -0500

    Add index

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-/*var testaverage = [5, 2, 7, 6, 2, 2, 6, 2, 8, 5, 5, 9, 1, 10, 2, 10];
-
-var sum = 0;
-var period = 10;
-for( var i = 0; i < period; i++ ){
-    sum += testaverage[testaverage.length-period+i];
-}
-
-var avg = sum/period;
-
-console.log(avg);*/
-
-/*function readTextFile(file, callback) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
-        }
-    }
-    rawFile.send(null);
-}
-
-//usage:
-readTextFile("ovitest.json", function(text){
-    var data = JSON.parse(text);
-    console.log(data["201914"]["Shots"]);
-});*/
-
 var originaldata = "ovitest.json";
 var request = new XMLHttpRequest();
 request.open('GET', originaldata);
@@ -38,7 +8,8 @@ request.onload = function() {
 	console.log(request.response);
 	var bunchadata = request.response;
 	var evshots = evShotsPer60(bunchadata);
-	console.log(evshots);
+	//console.log(evshots);
+	document.getElementById("evshots").innerHTML = evshots.toPrecision(4);
 }
 
 function evShotsPer60(jsonObj) {
@@ -74,22 +45,3 @@ function evShotsPer60(jsonObj) {
 	var avg = avgshots/avgtimehour;
 	return avg;
 }
-
-/*var foo = {
-	"201901": {
-		"Goals": 1,
-		"Shots": 5,
-	},
-	"201902": {
-		"Goals": 0,
-		"Shots": 7,
-	},
-	"201903": {
-		"Goals": 2,
-		"Shots": 2,
-	},
-}
-
-for (const shots in foo) {
-	console.log(foo[shots]["Shots"]);
-}*/
